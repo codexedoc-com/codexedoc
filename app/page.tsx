8d6c8d0fca2882082a975ae9ecab43b26ca71a43
Glenn Hensley III <glenniii.dev@gmail.com>
2026-05-26 09:47:02 -0400

    Init

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+        <h1 className="bg-linear-to-r from-indigo-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent font-black text-4xl">CODEXEDOC</h1>
+    </main>
+  );
+}
--- src-tauri/src/core/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod encryption;
-pub mod storage;
-pub mod vault;
-pub mod key_manager;
-pub mod models;
