d7751054bdf440057f49efcf7439d1e4d7ad53f1
Glenn Hensley III <glenniii.dev@gmail.com>
2026-06-08 08:47:07 -0400

    refactor: update import paths to use centralized app queries module (#21)

--- components/GoalCreationFlow.tsx
+++ components/GoalCreationFlow.tsx
@@ -3,7 +3,7 @@
 import { useState, useTransition } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { ArrowRight, ArrowLeft, Sparkles } from "lucide-react";
-import { createGoalAction } from "@/server/queries/dashboardQueries";
+import { createGoalAction } from "@/server/queries/app";
 
 interface GoalCreationFlowProps {
   onClose?: () => void;
